3db8dfb7f71653394db68b758497b07bb2b11b74
Wyfi0 <wyfi08g@proton.me>
2024-06-12 09:22:43 -0400

    added even more language servers

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -67,6 +67,41 @@ lspconfig.pylsp.setup({
 -- My configs taken from https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 ------------------
 
+-- lua whoooah config lol 
+require'lspconfig'.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if vim.loop.fs_stat(path..'/.luarc.json') or vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      return
+    end
+
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT'
+      },
+      -- Make the server aware of Neovim runtime files
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+          -- Depending on the usage, you might want to add additional paths here.
+          -- "${3rd}/luv/library"
+          -- "${3rd}/busted/library",
+        }
+        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+        -- library = vim.api.nvim_get_runtime_file("", true)
+      }
+    })
+  end,
+  settings = {
+    Lua = {}
+  }
+}
+
+
+
 -- rust_analyzer config Uwu
 require'lspconfig'.rust_analyzer.setup{
   settings = {
